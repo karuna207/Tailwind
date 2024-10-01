c465e2d756e3a7c319f4fac47f90c5ebf056bf0e
karuna207 <karunanidhiayyamperumal@gmail.com>
2024-10-01 19:01:36 +0530

    responsiveness

---
+++ fonts/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>  
+      <div className='ml-3 '>
+      <p className='hidden md:block text-black'>1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum rerum nemo quasi aliquid earum magnam tenetur et! Ab, blanditiis. Quisquam neque iste sunt tempore numquam aut officia qui pariatur cupiditate.</p> 
+      <p className='md:hidden text-black'>2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum aspernatur maxime eos esse sint nisi, id quas similique eum officia corrupti voluptas blanditiis ullam alias, magnam eveniet beatae ipsam nobis?</p>
+      
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
